e90a4680bac2e6b92aefbf57aedbaca2e489699a
xcob <34405172+xcob@users.noreply.github.com>
2019-08-28 07:55:14 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,233 @@
+
+window.sr = ScrollReveal();
+sr.reveal('#letterJ', {
+  duration: 1500,
+  origin: 'left',
+  distance: '500px'
+});
+sr.reveal('#letterA', {
+  duration: 1700,
+  origin: 'left',
+  distance: '500px'
+});
+sr.reveal('#letterC', {
+  duration: 1900,
+  origin: 'left',
+  distance: '500px'
+});
+sr.reveal('#letterO', {
+  duration: 2100,
+  origin: 'left',
+  distance: '500px'
+});
+sr.reveal('#letterB', {
+  duration: 2300,
+  origin: 'left',
+  distance: '500px'
+});
+sr.reveal('#letterS', {
+  duration: 1500,
+  origin: 'right',
+  distance: '500px'
+});
+sr.reveal('#letterM', {
+  duration: 1700,
+  origin: 'right',
+  distance: '500px'
+});
+sr.reveal('#letterI', {
+  duration: 1900,
+  origin: 'right',
+  distance: '500px'
+});
+sr.reveal('#letterT', {
+  duration: 2100,
+  origin: 'right',
+  distance: '500px'
+})
+sr.reveal('#letterH', {
+  duration: 2300,
+  origin: 'right',
+  distance: '500px'
+});
+sr.reveal('#explore', {
+  duration: 2500,
+  origin: 'left',
+  distance: '800px'
+});
+sr.reveal('#work1', {
+  duration: 1300,
+  origin: 'left',
+  distance: '100px'
+});
+sr.reveal('#work2', {
+  duration: 1500,
+  origin: 'left',
+  distance: '150px'
+});
+sr.reveal('#work3', {
+  duration: 1700,
+  origin: 'left',
+  distance: '200px'
+});
+sr.reveal('#work4', {
+  duration: 1900,
+  origin: 'left',
+  distance: '250px'
+});
+sr.reveal('#work5', {
+  duration: 2100,
+  origin: 'left',
+  distance: '300px'
+});
+sr.reveal('.work6', {
+  duration: 1000,
+  origin: 'left',
+  distance: '30px'
+});
+sr.reveal('.education', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '30px'
+});
+sr.reveal('.about-text', {
+  duration: 1200,
+  origin: 'left',
+  distance: '100px'
+});
+
+// For Previous work
+sr.reveal('#letter1', {
+  duration: 1200,
+  origin: 'left',
+  distance: '300px'
+});
+sr.reveal('#letter2', {
+  duration: 1200,
+  origin: 'top',
+  distance: '500px'
+});
+sr.reveal('#letter3', {
+  duration: 1300,
+  origin: 'bottom',
+  distance: '300px'
+});
+sr.reveal('#letter4', {
+  duration: 1300,
+  origin: 'right',
+  distance: '500px'
+});
+sr.reveal('#letter5', {
+  duration: 1100,
+  origin: 'left',
+  distance: '400px'
+});
+sr.reveal('#letter6', {
+  duration: 1200,
+  origin: 'top',
+  distance: '600px'
+});
+sr.reveal('#letter7', {
+  duration: 1200,
+  origin: 'right',
+  distance: '200px'
+});
+sr.reveal('#letter8', {
+  duration: 1200,
+  origin: 'bottom',
+  distance: '400px'
+});
+sr.reveal('#letter9', {
+  duration: 1200,
+  origin: 'left',
+  distance: '200px'
+});
+sr.reveal('#letter10', {
+  duration: 1200,
+  origin: 'right',
+  distance: '100px'
+});
+sr.reveal('#letter11', {
+  duration: 1200,
+  origin: 'top',
+  distance: '100px'
+});
+sr.reveal('#letter12', {
+  duration: 1400,
+  origin: 'left',
+  distance: '300px'
+});
+
+// Education section
+sr.reveal('#education1', {
+  duration: 2100,
+  origin: 'left',
+  distance: '900px'
+});
+sr.reveal('#education2', {
+  duration: 2000,
+  origin: 'left',
+  distance: '800px'
+});
+sr.reveal('#education3', {
+  duration: 1800,
+  origin: 'left',
+  distance: '700px'
+});
+sr.reveal('#education4', {
+  duration: 1600,
+  origin: 'left',
+  distance: '600px'
+});
+sr.reveal('#education5', {
+  duration: 1400,
+  origin: 'right',
+  distance: '500px'
+});
+sr.reveal('#education6', {
+  duration: 1600,
+  origin: 'right',
+  distance: '600px'
+});
+sr.reveal('#education7', {
+  duration: 1800,
+  origin: 'right',
+  distance: '700px'
+});
+sr.reveal('#education8', {
+  duration: 2000,
+  origin: 'right',
+  distance: '800px'
+});
+sr.reveal('#education9', {
+  duration: 2100,
+  origin: 'right',
+  distance: '900px'
+});
+
+// Education Places
+sr.reveal('#exp1', {
+  duration: 1500,
+  origin: 'bottom',
+  distance: '900px'
+});
+sr.reveal('#exp2', {
+  duration: 1600,
+  origin: 'bottom',
+  distance: '900px'
+});
+sr.reveal('#exp3', {
+  duration: 1700,
+  origin: 'bottom',
+  distance: '900px'
+});
+sr.reveal('#exp4', {
+  duration: 1800,
+  origin: 'bottom',
+  distance: '900px'
+});
+sr.reveal('#exp5', {
+  duration: 1900,
+  origin: 'bottom',
+  distance: '900px'
+});
